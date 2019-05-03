64e129c38760097f90908958d8d0e9e9a9fe822b
yangshuai <704807396@qq.com>
2019-05-03 12:04:23 +0800

    增加自动打包命令

--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,28 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 // // 抽离css样式
 // const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    // 模式 "production" | "development" | "none"
-    mode: 'none',
+// 从环境变量中换取 PACKAGE_NAME 值
+const PACKAGE_NAME = process.env.PACKAGE_NAME;
 
+const webpackConfig = {
+    // 模式 "production" | "development" | "none"
+    mode: 'production',
+
+    // 入口
     entry: {
-        // http
-        // "react-kit-http": './projects/react-kit/http/src/index.ts',
-        // auth
-        "react-kit-auth": './projects/react-kit/auth/src/index.ts'
     },
 
+    // 出口
     output: {
         filename: '[name].js',
-        // http
-        // path: path.resolve(__dirname, 'projects/react-kit/http/dist'),
-        // auth
-        path: path.resolve(__dirname, 'projects/react-kit/auth/dist'),
+        path: path.resolve(__dirname, 'projects/react-kit/' + PACKAGE_NAME + '/dist'),
         libraryTarget: 'umd'
     },
 
     // 解析
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        // 引入文件时候，可以不加后缀
+        extensions: ['.ts', '.tsx', '.js', ".json"]
     },
 
     // 以便发布调试
@@ -58,12 +57,30 @@ module.exports = {
     },
     externals: [WebpackNodeExternals()],
     plugins: [
-        new CleanWebpackPlugin([
-            'projects/react-kit/http/dist',
-            'projects/react-kit/auth/dist',
-        ]),
         // new ExtractTextPlugin({
         //     filename: 'index.css'
         // })
     ]
 };
+
+if (PACKAGE_NAME === 'http') {
+    webpackConfig.entry = {
+        "react-kit-http": './projects/react-kit/http/src/index.ts'
+    };
+    webpackConfig.plugins.push(
+        new CleanWebpackPlugin([
+            'projects/react-kit/http/dist'
+        ])
+    );
+} else if(PACKAGE_NAME === 'auth') {
+    webpackConfig.entry = {
+        "react-kit-auth": './projects/react-kit/auth/src/index.ts'
+    };
+    webpackConfig.plugins.push(
+        new CleanWebpackPlugin([
+            'projects/react-kit/auth/dist'
+        ])
+    );
+}
+
+module.exports = webpackConfig;
